92e4c74f300ffbfb360612545c349dab95b79416
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2023-11-14 20:52:59 -0500

    documentacion a las funciones se adiciono una redirecion del root "/" a docs

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 
-# from .routers import index as index_router
-from .routers import graph_route as graph_router
+from .routers import index as index_router
+from fastapi.responses import RedirectResponse
+
+# from .routers import graph_route as graph_router
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    """
+    Redirect to /docs.
+    """
+    return RedirectResponse(url="/docs")
 
 
-# app.include_router(index_router.router)
-app.include_router(graph_router.router)
+app.include_router(index_router.router)
+# app.include_router(graph_router.router)
--- app/routers/graph_route.py
+++ app/routers/graph_route.py
@@ -5,33 +5,34 @@ from app.utils.graph import Grafo
 grafo = Grafo()
 
 total_cargado = grafo.import_graph_from_file("app/assets/graph__file/grafo_anime.csv")
-print(total_cargado)
-grafo.print_graph()
+# print(total_cargado)
+# grafo.print_graph()
 
 router = APIRouter()
 
 
 @router.get("/graph")
 async def get_graph():
+    """
+    Returns a dictionary containing the graph data.
+
+    Returns:
+    dict: A dictionary containing the graph data.
+    """
     return {"graph": total_cargado}
 
 
 @router.get("/anime/two_recomend/{start}/{end}")
 async def dijkstra_shortest_path(start, end):
     """
-    Encuentra el camino mas corto entre dos recomendaciones de anime.
-    Parameters:
-        start (String): Nombre del anime
-        end (String): NOmbre del anime
+    Encuentra el camino más corto entre dos animes utilizando el algoritmo de Dijkstra.
+
+    Args:
+        start (str): El anime de inicio.
+        end (str): El anime de destino.
 
     Returns:
-        dict: A dictionary containing the recommended animes and the consulted animes.
-            - "animes_recomendados" (list): A list of recommended animes.
-            - "animes_consultados" (list): A list of consulted animes.
-
-        If no path is found, it returns:
-        dict: A dictionary containing the error message.
-            - "error" (str): The error message indicating that no path was found.
+        dict: Un diccionario que contiene los animes recomendados y los animes consultados.
     """
     short_path, _ = grafo.dijkstra_shortest_path(start, end)
     if short_path is None:
